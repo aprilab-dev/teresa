5c970424412551f0bcb892a560f5bfb8c22321de
Yuxiao QIN <yuxiao@terraqt.com>
2021-06-10 11:37:52 +0800

    added the basic structure for setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,40 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as f:
-    readme = f.read()
+with open("README.md") as fp:
+    readme = fp.read()
 
 # https://packaging.python.org/guides/single-sourcing-package-version/
 version_info = {}
-with open("teresa/version.py") as file:
-    exec(file.read(), version_info)
+with open("teresa/version.py") as fp:
+    exec(fp.read(), version_info)
 
+# dependencies
+with open("requirements.txt") as fp:
+    install_requires = fp.read()
+tests_require=[   # tests
+    "pytest",
+    "pytest-cov"
+]
+docs_require=[ # docs 
+    "sphinx",
+]
 
 setup(
     name="teresa",
     version=version_info["__version__"],
-    description="TErraquanta's software for REgistration and SAmpling.",
+    description="[TE]rraquanta's software for [RE]gistration and [SA]mpling",
     long_description=readme,
     author="TerraQuanta",
     url="https://git.terraqt.dev/dev.fringe/teresa",
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=install_requires,
+    setup_requires=["pytest-runner", "flake8"],
+    tests_require=tests_require,
+    extras_require={
+	"test": tests_require, 
+	"doc": docs_require
+    },
 )
 
