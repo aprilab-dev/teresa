f6cf21f09b46281095825be2e30c799c9a79909d
Yuxiao QIN <yuxiao@terraqt.com>
2021-06-11 18:33:34 +0800

    update the structure for coregister.py.

--- teresa/coregister.py
+++ teresa/coregister.py
@@ -2,6 +2,7 @@
 """
 
 import abc
+import processor
 
 class Coregister(abc.ABC):
     def __init__(self):
@@ -11,10 +12,30 @@ class Coregister(abc.ABC):
     def coregistration(self):
         pass
 
+
 class Sentinel1Coregister(Coregister):
     # concrete creater
-    def __init__(self):
-        pass
+    def __init__(self, processor: processor.Processor = processor.GptProcessor()):
+        self._processor = processor  # interface
+
+    def _prepare(self):
+        pass  # administritive stuff
+
+    def _coregister(self):
+        # the lofic for swath coregistration. 
+        # The concrete implementation is actually in _subswath_coregister. 
+        for nsubswath in range(0, 3):
+            self._subswath_coregister(nsubswath)
+
+    def _subswath_coregister(self, nsubswath):
+        # the logic for subswath coregistration
+        self._processor.process()  # concrete implementation
+
+    def _merge(self):
+        self._processor.process()
+
+    def _finalize(self):
+        pass # the logic for cleaning up
 
     def coregistration(
         self,
@@ -26,4 +47,16 @@ class Sentinel1Coregister(Coregister):
         dry_run:bool=False,
         **kwargs,
     ):
+        self._prepare()
+        self._coregister()
+        self._merge()
+        self._finalize()
+
+
+class TSXCoregister(Coregister):
+    def __init__(self):
         pass
+
+    def coregistration(self):
+        pass
+
--- teresa/processor.py
+++ teresa/processor.py
@@ -5,8 +5,9 @@ from config import Config, LocalConfig
 from typing import Union
 
 
-class GptError(RuntimeError):  
+class GptError(RuntimeError):
     pass  # categorize GPTERROR
+
 
 class Processor(abc.ABC):
     @abc.abstractmethod
@@ -25,13 +26,7 @@ class GptProcessor(Processor):
         self._cache_size = cache_size
         self._cores = cores
 
-    def process(
-        self, 
-        graph: str, 
-        dry_run: bool = False, 
-        debug: bool = False, 
-        **kwargs
-    ):
+    def process(self, graph: str, dry_run: bool = False, debug: bool = False, **kwargs):
         params = ["{} {} ".format(self._executable, graph)]
         params += ["-P{}='{}' ".format(name, value) for name, value in kwargs.items()]
         params.append("-c {} -q {} ".format(self._cache_size, self._cores))
@@ -46,5 +41,15 @@ class GptProcessor(Processor):
             subprocess.run(command, shell=True, check=True, stderr=subprocess.PIPE)
         except subprocess.CalledProcessError as e:
             raise GptError(
-                "GPT command failed \n {} \n Stderr: {}".format(command, e.stderr.decode("utf-8"))
+                "GPT command failed \n {} \n Stderr: {}".format(
+                    command, e.stderr.decode("utf-8")
+                )
             ) from e
+
+
+class DorisProcessor(Processor):  # leave room for other sensors
+    def __init__(self):
+        pass
+
+    def process(self):
+        pass
