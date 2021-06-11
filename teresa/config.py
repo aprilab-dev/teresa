bd6f61d96d847b191435321491cae3d73e5a4603
Yuxiao QIN <yuxiao@terraqt.com>
2021-06-11 14:52:07 +0800

    set up the rprocessor using GPT.

--- teresa/config.py
+++ teresa/config.py
@@ -1,12 +1,14 @@
 import os
 
+
 class Config:
     # https://github.com/microsoftgraph/python-sample-auth/blob/master/config.py
     # https://github.com/amundsen-io/amundsenfrontendlibrary/blob/master/amundsen_application/config.py
     SNAP_GPT_EXECUTABLE = os.getenv("SNAP_GPT_EXECUTABLE", None)
 
+
 class LocalConfig(Config):
 
     LOGGING_LEVEL = "DEBUG"
     SNAP_GPT_CACHE_SIZE = "16G"
-    SNAP_GPT_CORES = os.cpu_count()
+    SNAP_GPT_NTHREADS = os.cpu_count()
--- teresa/processor.py
+++ teresa/processor.py
@@ -1,22 +1,50 @@
 import abc
+import logging
+import subprocess
+from config import Config, LocalConfig
+from typing import Union
+
+
+class GptError(RuntimeError):  
+    pass  # categorize GPTERROR
 
 class Processor(abc.ABC):
-
     @abc.abstractmethod
     def process(self):
         pass
 
+
 class GptProcessor(Processor):
+    def __init__(
+        self,
+        executable: str = Config.SNAP_GPT_EXECUTABLE,
+        cache_size: str = LocalConfig.SNAP_GPT_CACHE_SIZE,
+        cores: int = LocalConfig.SNAP_GPT_NTHREADS,
+    ):
+        self._executable = executable
+        self._cache_size = cache_size
+        self._cores = cores
 
-    def __init__(self):
-        pass
+    def process(
+        self, 
+        graph: str, 
+        dry_run: bool = False, 
+        debug: bool = False, 
+        **kwargs
+    ):
+        params = ["{} {} ".format(self._executable, graph)]
+        params += ["-P{}='{}' ".format(name, value) for name, value in kwargs.items()]
+        params.append("-c {} -q {} ".format(self._cache_size, self._cores))
+        if debug:
+            params.append("-e")
+        command = "".join(params)
 
-    def process(self):
-        pass
-
-class DorisProcessor(Processor):
-    def __init__(self):
-        pass
-
-    def process(self):
-        pass
+        try:
+            logging.info("Run: {}".format(command))
+            if dry_run:
+                return
+            subprocess.run(command, shell=True, check=True, stderr=subprocess.PIPE)
+        except subprocess.CalledProcessError as e:
+            raise GptError(
+                "GPT command failed \n {} \n Stderr: {}".format(command, e.stderr.decode("utf-8"))
+            ) from e
