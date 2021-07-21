e5d1068bec1b31fbfbca80be9996048dc1b60de2
Yuxiao QIN <yuxiao@terraqt.com>
2021-07-21 20:36:14 +0800

    WIP: implement _coregister_subswath().

--- teresa/coregistration.py
+++ teresa/coregistration.py
@@ -1,30 +1,37 @@
 import abc
 import processor
-from typing import Union
 from . import stack
 
 
 class Coregistration(abc.ABC):
-    def __init__(self):
-        pass
 
     @abc.abstractmethod
     def coregister(self):
-        pass
-
+        ...
 
 class Sentinel1Coregistration(Coregistration):
     # concrete creater
-    def __init__(self, processor: processor.GptProcessor = processor.GptProcessor()):
-        self._processor = processor  # interface
+    def __init__(
+        self,
+        slc_pair: stack.SlcPair,
+        output_path: str,
+        polarization: str = "vv",
+        dry_run: bool = False,
+        coreg_processor: processor.GptProcessor = processor.GptProcessor(),
+    ):
         self.graph = "None"
+        self.slc_pair = slc_pair
+        self.output_path = output_path
+        self.polarization = polarization
+        self.dry_run = dry_run
+        self._processor = coreg_processor  # interface
 
     def _prepare(self):
-        # 1. select graph based on input meta. 
+        # 1. select graph based on input meta.
         self._select_graph("dummy_graph.xml")  # administritive stuff
 
     def _select_graph(self, graph: str):
-        """Implement the logic for selecting the correct graph for processing. 
+        """Implement the logic for selecting the correct graph for processing.
 
         Parameters
         ----------
@@ -34,36 +41,48 @@ class Sentinel1Coregistration(Coregistration):
         self.graph = graph  # input meta and output graph(xml)
 
     def _coregister(self):
-        # the lofic for swath coregistration. 
-        # The concrete implementation is actually in _subswath_coregister. 
+        # the lofic for swath coregistration.
+        # The concrete implementation is actually in _subswath_coregister.
         for nsubswath in range(0, 3):
             self._coregister_subswath(nsubswath)
 
     def _coregister_subswath(self, nsubswath):
-        """Implement the logic for coregistering by calling GPT. 
-
-        Parameters
-        ----------
-        nsubswath : [type]
-            [description]
-        """
+        """TODO: add description."""
         self._processor.process(
             self.graph,
-            dry_run=True, 
+            dry_run=True,
         )  # concrete implementation
+
+        # graph = self._get_subswath_coregistration_graph(secondary, reference, esd=esd)
+
+        # Execute the actual coregistration
+        self._processor.process(
+            self.graph,
+            subswath=f"IW{nsubswath}",
+            polorization=self.polarization.upper(),
+            master_file=self.slc_pair.master.file,
+            slave_file=self.slc_pair.slave.file,
+            output_path=self.output_path,
+            dry_run=self.dry_run,
+        )
+
+        if self.dry_run:
+            # do something to show the dry run result
+            pass
+
+        return True
 
     def _merge(self):
         self._processor.process(self.graph)
 
     def _finalize(self):
-        pass # the logic for cleaning up
+        pass  # the logic for cleaning up
 
     def coregister(
         self,
-        master:stack.Image,
-        slave:stack.Image,
-        dry_run:bool=False,
-        **kwargs,
+        master: stack.Image,
+        slave: stack.Image,
+        dry_run: bool = False,
     ):
         self._prepare()
         self._coregister()
@@ -77,4 +96,3 @@ class TSXCoregistration(Coregistration):
 
     def coregistration(self):
         pass
-
--- teresa/stack.py
+++ teresa/stack.py
@@ -1,10 +1,12 @@
 
 
 class Image:
-    def __init__(self):
-        pass
+    def __init__(self, date: str, filepath:str):
+        self.date = date
+        self.file = filepath
 
 class SlcPair:
     def __init__(self, master: Image, slave: Image):
         self.master = master
         self.slave = slave
+
