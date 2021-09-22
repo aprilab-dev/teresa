65339dce1a9c492956293e2f713b8b183e1eb4b4
Yuxiao QIN <yuxiao.qin@nwpu.edu.cn>
2021-09-23 00:29:25 +0800

    refactored helpers.py.

--- teresa/helpers.py
+++ teresa/helpers.py
@@ -1,22 +1,32 @@
-import pypinyin
+from pypinyin import lazy_pinyin as pinyin
 import requests
+from log import log_config
 
+logger = log_config()
 
 API_KEY = "B5EBZ-W2GCI-Q23GO-5RJTM-YRZL7-WJBHO"
 
 
-def lat_lon_to_city(lat: float, lon: float) -> str:
-    """Convert a point with latitude and longitude to its nearest city"""
+def latlon_to_city(lat: float, lon: float) -> str:
+    """Convert a point with latitude and longitude to its nearest city."""
+
+    url = "https://apis.map.qq.com/ws/geocoder/v1/?location={},{}&key={}".format(
+        lat, lon, API_KEY
+    )
+    
     try:
-        url_request = "https://apis.map.qq.com/ws/geocoder/v1/?location={},{}&key={}&get_poi=1".format(
-            lat, lon, API_KEY
+        content = requests.get(url, timeout=60).json()
+    except requests.exceptions.Timeout:
+        return  "TIMEOUT"
+
+    if content["status"] != 0:
+        logger.error(
+            "ERROR %s: https://lbs.qq.com/service/webService/webServiceGuide/status",
+            content["message"],
         )
-        data_flow = requests.get(url_request, timeout=120)
-        text_json = data_flow.json()
-        address_component = text_json["result"]["address_component"]
-        city_nearest = address_component["city"]
-        city_nearest_pinyin_list = pypinyin.lazy_pinyin(city_nearest)
-        city_nearest_pinyin = "".join(city_nearest_pinyin_list)
-        return city_nearest_pinyin
-    except:
-        print("time out!")
+    if content["result"]["ad_info"]["nation_code"] != "156":
+        return "ABOARD"
+    
+    nearest_city = pinyin(content["result"]["address_component"]["city"])
+    nearest_city = nearest_city[:-1] if nearest_city[-1] == "shi" else nearest_city
+    return "cn_" + "".join(nearest_city)
