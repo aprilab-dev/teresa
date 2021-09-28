2dd54a2c50246d9820d92edfc69b8e1805d42883
Yuxiao QIN <yuxiao@terraqt.com>
2021-09-28 21:52:02 +0800

    First working exampling of pruning.

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,61 +1,14 @@
 import os
 import pytest
-from teresa import stack, log
-
-
-def create_multiple_masters_multiple_slaves(tmpdir):
-    test_slc_dir = tmpdir.mkdir("multiple_masters_multiple_slaves")
-    slcs = [
-        "S1A_IW_SLC__1SDV_20210507T104527_20210507T104553_037781_047584_C9C5",
-        "S1A_IW_SLC__1SDV_20210507T104502_20210507T104529_037781_047584_0263",
-        "S1A_IW_SLC__1SDV_20210519T104527_20210519T104554_037956_047AD0_1750",
-        "S1A_IW_SLC__1SDV_20210519T104502_20210519T104529_037956_047AD0_D69B",
-    ]
-    for slc in slcs:
-        test_slc_dir.join(slc).write("multiple masters, multiple slaves")
-    return test_slc_dir, {"20210507": 2, "20210519": 2}
-
-
-def create_multiple_masters_single_slave(tmpdir):
-    test_slc_dir = tmpdir.mkdir("multiple_masters_single_slave")
-    slcs = [
-        "S1A_IW_SLC__1SDV_20210507T104527_20210507T104553_037781_047584_C9C5",
-        "S1A_IW_SLC__1SDV_20210507T104502_20210507T104529_037781_047584_0263",
-        "S1A_IW_SLC__1SDV_20210519T104527_20210519T104554_037956_047AD0_1750",
-    ]
-    for slc in slcs:
-        test_slc_dir.join(slc).write("multiple masters, single slaves")
-    return test_slc_dir, {"20210507": 2, "20210519": 1}
-
-
-def create_single_master_multiple_slaves(tmpdir):
-    test_slc_dir = tmpdir.mkdir("single_master_multiple_slaves")
-    slcs = [
-        "S1A_IW_SLC__1SDV_20210507T104527_20210507T104553_037781_047584_C9C5",
-        "S1A_IW_SLC__1SDV_20210519T104527_20210519T104554_037956_047AD0_1750",
-        "S1A_IW_SLC__1SDV_20210519T104502_20210519T104529_037956_047AD0_D69B",
-    ]
-    for slc in slcs:
-        test_slc_dir.join(slc).write("single master, multiple slaves")
-    return test_slc_dir, {"20210507": 1, "20210519": 2}
-
-
-def create_single_master_single_slave(tmpdir):
-    test_slc_dir = tmpdir.mkdir("single_master_single_slave")
-    slcs = [
-        "S1A_IW_SLC__1SDV_20210507T104527_20210507T104553_037781_047584_C9C5",
-        "S1A_IW_SLC__1SDV_20210519T104527_20210519T104554_037956_047AD0_1750",
-    ]
-    for slc in slcs:
-        test_slc_dir.join(slc).write("single master, single slave")
-    return test_slc_dir, {"20210507": 1, "20210519": 1}
+from teresa import stack, log, coregistration
+from . import conftest
 
 
 mocked_s1_slcstack = [
-    create_multiple_masters_multiple_slaves,
-    create_multiple_masters_single_slave,
-    create_single_master_multiple_slaves,
-    create_single_master_single_slave,
+    conftest.create_multiple_masters_multiple_slaves,
+    conftest.create_multiple_masters_single_slave,
+    conftest.create_single_master_multiple_slaves,
+    conftest.create_single_master_single_slave,
 ]
 
 
@@ -74,7 +27,7 @@ def test_sentinel1_slcstack_coregister(tmpdir, mocked):
     tmp_stack = stack.Sentinel1SlcStack(sourcedir=source_dir).load()
     tmp_stack.coregister(master="20210507", output=source_dir)
     # check if output is in the log
-    assert os.path.join(source_dir, "coregistration") in open(log.LOG_FNAME).read()
+    assert os.path.join(source_dir, coregistration.COREG_DIR) in open(log.LOG_FNAME).read()
 
 
 def test_slcimage_append():
