1097fbec4eecce91527b1515dcd25f0c03f62794
junjun-gao <gao.junjun@outlook.com>
2026-03-19 17:48:27 +0800

    画图函数优化

--- teresa/utils/plotTools.py
+++ teresa/utils/plotTools.py
@@ -2,18 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-dtype_format_map = {
-    "complex_short": np.complex64,   # 由两个 int16 表示 (I/Q)，常用于SAR数据
-    "short": np.int16,
-    "int": np.int32,
-    "long": np.int64,
-    "float": np.float32,
-    "double": np.float64,
-    "complex_float": np.complex64,
-    "complex_double": np.complex128
-}
 
-def plot_amplitude(filename, output_png, nlines, npixels, dtype, downsample_factor=2):
+def plot_amplitude(filename, nlines, npixels, downsample_factor=2):
     """
     读取 Doris 输出的复数文件，计算并保存幅度和相位图像。
 
@@ -25,24 +15,43 @@ def plot_amplitude(filename, output_png, nlines, npixels, dtype, downsample_fact
     - downsample_factor: int, 下采样因子，默认 2
     """
     # 读入数据
-    data = np.fromfile(filename, dtype=dtype)
+    data = np.fromfile(filename, dtype=np.int16)
     
     # 每个像素由 (real, imag) 两个 float32 构成
-    data = data.reshape((nlines, npixels, 2))
-    cpx = data[:,:,0] + 1j * data[:,:,1]
-
-    # 计算幅度
-    amp = np.abs(cpx)
-    amp_norm = (amp / np.max(amp) * 255).astype(np.uint8)
+    data = data.reshape(nlines, npixels, 2)
+    complex_img = data[:,:,0] + 1j * data[:,:,1]
 
     # 下采样
-    amp_down = amp_norm[::downsample_factor, ::downsample_factor]
+    complex_img = complex_img[::downsample_factor, ::downsample_factor]
+
+    # 计算幅度
+    amplitude = np.abs(complex_img)
+    amp_display = np.log1p(amplitude)
+
+    # 计算相位
+    phase = np.angle(complex_img)
+    phase_norm = ((phase + np.pi) / (2 * np.pi) * 255).astype(np.uint8)
+
+    # plt.imsave("amplitude.png", amp_display, cmap="gray")
+    # plt.imsave("phase.png", phase_norm, cmap="hsv", vmin=0, vmax=255)
+
 
     # 保存 PNG
-    plt.imsave(output_png, amp_down, cmap="gray")
+    plt.figure(figsize=(6,6))
+    plt.imshow(amp_display, cmap='gray')
+    plt.axis('off')
+    plt.savefig("amplitude.png", dpi=300, bbox_inches='tight')
+
+    plt.figure(figsize=(6,6))
+    plt.imshow(phase_norm, cmap='hsv', vmin=-np.pi, vmax=np.pi)
+    plt.colorbar()
+
+    plt.axis('off')
+    plt.savefig("phase.png", dpi=300, bbox_inches='tight', pad_inches=0)
+    plt.close()
 
 
-def plot_phase(filename, output_png, nlines, npixels, dtype, downsample_factor=2):
+def plot_phase(filename, output_png, nlines, npixels, dtype, downsample_factor=4):
     """
     读取 Doris 输出的复数文件，计算并保存相位图像。
 
@@ -72,11 +81,9 @@ def plot_phase(filename, output_png, nlines, npixels, dtype, downsample_factor=2
 
 
 if __name__ == "__main__":
-    num_of_lines  = 15918
-    num_of_pixels = 12040
-    dtype         = dtype_format_map["complex_real4"]
-    filename      = "/data/tests/junjun/coregTest/bc3/workspace/20240415/cint.minrefpha.raw"
-    output_png    = "/data/tests/junjun/coregTest/bc3/workspace/20240415/minrefpha.png"
+    num_of_lines  = 17920
+    num_of_pixels = 20736
+    filename      = "/data/tests/jinting/changsha/insar/stack/process_pro/workspace/20240309/slave_rsmp.raw"
 
-    plot_amplitude(filename, output_png, num_of_lines, num_of_pixels, dtype)
+    plot_amplitude(filename, num_of_lines, num_of_pixels)
     # plot_phase(filename, num_of_lines, num_of_pixels, dtype)
